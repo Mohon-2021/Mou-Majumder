80b49a7f600e2de0320692e842749d0b8ec0347e
MOU <moumajumder252@gmail.com>
2025-01-30 21:55:01 -0500

    Branch Mohon

---
+++ OurGitFolderTest/OurSelenium/src/BranchMohon.java
@@ -0,0 +1,10 @@
+
+public class BranchMohon {
+
+	public static void main(String[] args) {
+		System.out.println("I am Mohon");
+		System.out.println("I love to sing a song");
+
+	}
+
+}
---
+++ OurSelenium/src/ourJavaTest/GitPractice.java
@@ -0,0 +1,12 @@
+package ourJavaTest;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+	System.out.println("Git is not easy");
+	System.out.println("We need lots of practice");
+	System.out.println("But if we practice else we can do that"); 
+
+	}
+
+}
